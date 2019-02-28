921187d977e5699166ce281db3d9b355965e89a1
Micke29 <mickael9103@hotmail.fr>
2019-02-28 11:04:54 +0100

    affichage de laliste des posts avec le nombre de commentaire

--- model/PostManager.php
+++ model/PostManager.php
@@ -8,7 +8,7 @@ class PostManager extends Manager
 	public function getPosts()
 	{
 		$db = $this->dbConnect();
-		$req = $db->query('SELECT pst_id, pst_title, pst_content, DATE_FORMAT(pst_date, \'%d/%m/%Y à %Hh%imin\') AS pst_date_fr, pic_link, pic_title FROM t_posts_pst NATURAL JOIN t_picture_pic ORDER BY pst_id ASC');
+		$req = $db->query('SELECT pst_id, pst_title, pst_content, DATE_FORMAT(pst_date, \'%d/%m/%Y à %Hh%imin\') AS pst_date_fr, pic_link, pic_title, COUNT(cmt_id) AS cmt_number FROM t_posts_pst NATURAL JOIN t_picture_pic NATURAL JOIN t_comments_cmt GROUP BY pst_id ORDER BY pst_id ASC');
 
 		return $req;
 	}
--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -13,38 +13,9 @@ while($post = $posts->fetch())
 
     <div class="w3-container">
       <p class="w3-justify"><?= htmlspecialchars($post['pst_content']) ?></p>        
-      <p class="w3-left"><button class="w3-button w3-padding-large w3-white w3-border"><a class="w3-large">AFFICHER PLUS <i class="fa fa-angle-double-right"></i></a></button></p>           
-      <p class="w3-right"><button class="w3-button w3-black" onclick="myFunction('commentArea<?= $post['pst_id'] ?>')"><b>Commentaire(s) </b><span class="w3-tag w3-white">3</span></button></p>
+      <p class="w3-left"><a href="index.php?action=post&id=<?= $post['pst_id'] ?>"><button class="w3-button w3-padding-large w3-white w3-border w3-large">AFFICHER PLUS <i class="fa fa-angle-double-right"></i></button></a></p>           
+      <p class="w3-right"><a href="index.php?action=post&id=<?= $post['pst_id'] ?>#commentArea"><button class="w3-button w3-black"><b>Commentaire(s) </b><span class="w3-tag w3-white"><?= $post['cmt_number'] ?></button></a></span></p>
       <p class="w3-clear"></p>
-  
-      <!-- Example of comment field -->
-      <div id="commentArea<?= $post['pst_id'] ?>" style="display:none">
-        <hr>
-      <?php
-      $comments = $commentManager->getComments($post['pst_id']);
-      while($comment = $comments->fetch())
-      {
-      ?>
-        <div class="w3-row w3-margin-bottom">
-          <div class="w3-col l10 m9">
-            <h4><?= $comment['cmt_author'] ?> <span class="w3-opacity w3-medium"><?= $comment['cmt_date_fr'] ?></span></h4>
-            <p><?= $comment['cmt_content'] ?></p>
-          </div>
-        </div>
-      <?php
-      }
-      $comments->closeCursor();
-      ?>
-      </div>
-  
-      <hr>
-      <div class="w3-margin-bottom">
-        <form>
-          <input class="w3-input" type="text" placeholder="Prénom" required><br>
-          <textarea class="w3-input" placeholder="Commentaire" required></textarea><br>
-          <input class="w3-button w3-black" type="submit" value="Commenter">
-        </form>
-      </div>
     </div>
   </div>
   <hr>
