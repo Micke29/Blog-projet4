e40503eb7da4f41e5754d484928f4d1c4a994ab1
Mickaël Massé <masse.mickael1@gmail.com>
2019-03-11 16:59:19 +0100

    modifications diverses

--- view/backend/addArticleView.php
+++ view/backend/addArticleView.php
@@ -1,11 +1,10 @@
 <?php
 ob_start();
 ?>
-<form class="w3-container w3-center" method="post" action="" enctype="multipart/form-data">
+<form class="w3-container w3-center" method="post" enctype="multipart/form-data">
 	<div class="w3-container w3-margin-top w3-center w3-pale-red w3-text-deep-orange <?= isset($_SESSION['file']) ? 'w3-show-inline-block' : 'w3-hide' ?>" style="width:45%">
-		<p>Problème avec la photo, erreur <?= $_SESSION['file'] ?></p>
 	    <?php unset($_SESSION['file']); ?>
-	    
+	    <p>Problème avec la photo</p>	    
 	</div>
 
 	<h2 class="w3-xlarge">Création d'un article</h2>
@@ -17,7 +16,7 @@ ob_start();
 	<div class="w3-show-inline-block" style="width:90%">
 		<br><br><textarea class="tinymce w3-hide" name="content"></textarea>
 	</div>
-	<button class="w3-button w3-show-inline-block w3-black w3-section w3-padding" type="submit" style="width:20%">Ajouter</button>
+	<button class="w3-button w3-black w3-section w3-padding-small" type="submit" style="width:20%">Ajouter</button>
 </form>
 <?php
 $content = ob_get_clean();
