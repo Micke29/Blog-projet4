005487b18bb2dab666586eb6b49353d7ba1f2e46
Mickaël Massé <masse.mickael1@gmail.com>
2019-04-02 09:57:36 +0200

    Gestion de l'erreur 403

---
+++ error/403.php
@@ -0,0 +1,11 @@
+<?php
+ob_start();
+?>
+<div class="w3-center">
+	<h2 class="w3-padding">Accès refusé</h2>
+	<a href="./"><button class="w3-button w3-black"><i class="fa fa-home"></i>&nbsp;&nbsp;Retour à l'Accueil</button></a>
+</div>
+<?php
+$content = ob_get_clean();
+
+require('../view/frontend/template.php');
