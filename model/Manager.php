985b349a42916ae18b14bd9cc960b53bb47e3fdd
Mickaël Massé <masse.mickael1@gmail.com>
2019-04-04 14:33:43 +0200

    gestion creation des instance PDO

--- model/Manager.php
+++ model/Manager.php
@@ -3,9 +3,13 @@ namespace OpenClassrooms\Blog\Model;
 
 class Manager
 {
+	private static $_instance;
+
 	protected function dbConnect()
 	{
-		$db = new \PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', 'root');
-		return $db;
+		if (!isset(self::$_instance)) {
+			self::$_instance = new \PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', 'root');
+		}
+		return self::$_instance;
 	}
 }
